15235ae0b7e4f1e5f6d06621ada35b5240842ca2
Ana Arsovska <anamarsovska@gmail.com>
2018-01-20 21:37:47 +0100

    Fitting a regressor to the training set

---
+++ SimpleLinearRegression.py
@@ -0,0 +1,19 @@
+#Simple linear regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
